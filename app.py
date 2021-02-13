83f0ef7a34cc463161da294bc0b86b8f5cdce42b
Stacy Wandrey <Stacy.Wandrey@ucdconnect.ie>
2021-02-13 23:39:54 +0000

    progress is progress no matter how small

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 from flask_basicauth import BasicAuth
+from requests.auth import HTTPBasicAuth
+
 import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/json', methods=['GET'])
+def getJson():
+    result = requests.get('http://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/clients', auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
+    result2 = requests.get('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client?firstName=John', auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
+    print(result2)
+    print(result2.json())
+    return result2.json()
--- static/JavaScript/JS.js
+++ static/JavaScript/JS.js
@@ -3,5 +3,17 @@ function clientSearch(){
     let infoType = document.getElementById("dropdownmenu");
     let infoType2 = infoType.options[infoType.selectedIndex].value; 
     console.log(clientInfo);
-    console.log(infoType2);
+    console.log(infoType2)
 }
+
+$(document).ready(function(){
+    $.getJson("/json",function(data){
+        console.log(data.result2);
+        let items =  [];
+        $.each(data.result2, function(key, value){
+        console.log(key);
+        console.log(value);
+        items.push('<li id=" ' + value.firstName + '">' + value.lastName+    '</li>');
+    });
+})
+})
