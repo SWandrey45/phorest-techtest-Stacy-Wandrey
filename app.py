3178d6334d56d3bc73bed55bca62466575404913
Stacy Wandrey <Stacy.Wandrey@ucdconnect.ie>
2021-02-15 11:30:58 +0000

    successfully displayed card info

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template
 from flask_basicauth import BasicAuth
 from requests.auth import HTTPBasicAuth
+import simplejson as json
+from flask import jsonify
+
 
 import requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/json', methods=['GET'])
-def getJson():
-    result = requests.get('http://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/clients', auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
-    result2 = requests.get('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client?firstName=John', auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
-    print(result2)
-    print(result2.json())
-    return result2.json()
+
+
+# @app.route('/json', methods=['GET'])
+# def getJson(customerType, customerInfo):
+#     result2 = requests.get('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client?firstName=John', auth=('global/cloud@apiexamples.com', 'VMlRo/eh+Xd8M~l'))
+#     print(result2)
+#     print(json.dumps(result2))
+#     return 
--- static/JavaScript/JS.js
+++ static/JavaScript/JS.js
@@ -1,19 +1,62 @@
 function clientSearch(){
     let clientInfo = document.getElementById("input").value;
     let infoType = document.getElementById("dropdownmenu");
-    let infoType2 = infoType.options[infoType.selectedIndex].value; 
+    let infoTypeChosen = infoType.options[infoType.selectedIndex].value; 
     console.log(clientInfo);
-    console.log(infoType2)
-}
+    console.log(infoTypeChosen)
 
-$(document).ready(function(){
-    $.getJson("/json",function(data){
-        console.log(data.result2);
-        let items =  [];
-        $.each(data.result2, function(key, value){
-        console.log(key);
-        console.log(value);
-        items.push('<li id=" ' + value.firstName + '">' + value.lastName+    '</li>');
-    });
+
+var username = 'global/cloud@apiexamples.com';
+var password = 'VMlRo/eh+Xd8M~l';
+var url = 'http://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client?' + infoTypeChosen + '=' + clientInfo;
+ 
+$.ajax({ 
+    url: url,
+    async: true,
+    type:'GET',
+    dataType: 'json',
+    contentType: 'application/json',
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", "Basic "+btoa(username+':'+password));
+    },
+    success: function(json){
+        console.log(json)
+    },
+    error: function(err) {
+        console.log(err)
+    }
+}).done(function (clientJSON) {
+    let oldJSON = JSON.stringify(clientJSON);
+    let parsedJSON = JSON.parse(oldJSON);
+    let object = parsedJSON._embedded;
+    let clients = object.clients;
+    console.log(clients)
+
+    let ok = " <div class='card' margin-top:'50px'> <div class='card-body' style='background-color: white; margin-top: 50px;'> <h4 class='card-header'>Customer Information</h4><p class='card-text'>";
+    for(let i = 0; i < clients.length; i++){
+        clientID = clients[i].clientId
+
+        clientID = clients[i].clientId
+        if(clientInfo == clients[i].firstName || clientInfo == clients[i].lastName || clientInfo == clients[i].mobile || clientInfo == clients[i].email){
+        ok += "<p> Name: " + clients[i].firstName +  ' ' + clients[i].lastName + "</p>";
+        ok += "<p> Mobile: " + clients[i].mobile + "</p>"; 
+        ok += "<p> Email: " + clients[i].email + "</p>";
+        ok += "<button style='background-color:#ef9812; color:white;'>Add Voucher</button>"
+        ok += "</p></div> </div>"
+        console.log(clientID)
+        return clientID
+        } else{
+            console.log('no')
+            document.getElementById("clientCard").innerHTML = "Soory! No results! ";
+        }
+
+    }
+    document.getElementById("clientCard").innerHTML = ok;
+
 })
-})
+
+};
+
+function generateVoucher(){
+    
+}
