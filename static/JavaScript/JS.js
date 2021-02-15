ceed0f37f88f8bb0d8bce4b1079a920b97301c1b
Stacy Wandrey <Stacy.Wandrey@ucdconnect.ie>
2021-02-15 23:58:54 +0000

    starting to do the voucher bit, cleaned up UI

--- static/JavaScript/JS.js
+++ static/JavaScript/JS.js
@@ -20,7 +20,7 @@ $.ajax({
         xhr.setRequestHeader("Authorization", "Basic "+btoa(username+':'+password));
     },
     success: function(json){
-        console.log(json)
+        console.log("Yay! Success")
     },
     error: function(err) {
         console.log(err)
@@ -34,23 +34,24 @@ $.ajax({
 
     let ok = " ";
     for(let i = 0; i < clients.length; i++){
-
         clientID = clients[i].clientId
-
-        if(clientInfo == clients[i].firstName || clientInfo == clients[i].lastName || clientInfo == clients[i].mobile || clientInfo == clients[i].email){
-        ok += "<div class='col-md-6'> <div class='card' margin-top:'50px'> <div class='card-body'> <h4 class='card-header'>Customer: "+ clients[i].firstName + " " + clients[i].lastName + "</h4><p class='card-text'>"    
-        // ok += "<p> Name: " + clients[i].firstName +  ' ' + clients[i].lastName + "</p>";
+        if(clientInfo.toLowerCase() === clients[i].firstName.toLowerCase() || clientInfo.toLowerCase() === clients[i].lastName.toLowerCase() || clientInfo === clients[i].mobile || clientInfo === clients[i].email){
+        ok += "<div class='col-md-6'> <div class='card' margin-top:'50px'> <div class='card-body'> <h4 class='card-header'>Client: "+ clients[i].firstName + " " + clients[i].lastName + "</h4><p class='card-text'>"    
         ok += "<p> Mobile: " + clients[i].mobile + "</p>"; 
         ok += "<p> Email: " + clients[i].email + "</p>";
         ok += "<input type='text' id='voucherInput' class='form-control' placeholder='Enter Voucher Amount' aria-label='Enter Name or Phone Number' aria-describedby='basic-addon2'/>"
-        ok += "<button onClick='generateVoucher()' style='background-color:#ef9812; color:white;'>Add Voucher</button>"
-        ok += "</p></div></div></div>"
+        ok += "<button class='btn-sm' onClick='generateVoucher()' style='background-color:#ef9812; color:white;'>Add Voucher</button>"
+        ok += "<div style='color: white;'>   .  </div></p></div></div></div>"
         console.log(clientID)
-        } else{
-            ok += "Sorry, no results for that search, please try again! Search is case sensitive :) "
-            ok += "</div><div>"
+         } 
+        else if(clientInfo.toLowerCase() !== clients[i].firstName.toLowerCase() || clientInfo.toLowerCase() !== clients[i].lastName.toLowerCase() || clientInfo !== clients[i].mobile || clientInfo !== clients[i].email || clientInfo === ""){
+            //  ok = " ";
+             ok += "Sorry, no results for that search, please try again! Search is case sensitive :) ";
             console.log('no')
-            // document.getElementById("clientCard").innerHTML = "Soory! No results! ";
+        } else{
+            // ok = " "
+             ok += "Sorry, no results for that search, please try again! Search is case sensitive :) "
+             document.getElementById("clientCard").innerHTML = ok;
         }
 
     }
@@ -59,36 +60,34 @@ $.ajax({
 })
 
 };
-const username = 'global/cloud@apiexamples.com';
-const password = 'VMlRo/eh+Xd8M~l';
-const url = "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/voucher";
-
-// voucherInfo = '{ \"clientId\":' \"WwEaIb0m4bhJphVtm2VgIw\", \"creatingBranchId\": \"SE-J0emUgQnya14mOGdQS\", \"expiryDate\": \"2021-02-15T11:00:48.246Z\", \"issueDate\": \"2021-02-15T11:00:48.246Z\", \"originalBalance\": 123.12, \"serialNumber\": 123456}"
 
 function generateVoucher(){
-    $.ajax({ 
-        url: url,
-        async: true,
-        type:'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        voucherInfo: voucherInfo,
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Basic "+btoa(username+':'+password));
-        },
-        success: function(json){
-            console.log(json)
-        },
-        error: function(err) {
-            console.log(err)
-        }
-    }).done(function (voucherJSON) {
-        let oldJSON = JSON.stringify(voucherJSON);
-        let parsedJSON = JSON.parse(oldJSON);
-        console.log(parsedJSON)
-        // let object = parsedJSON._embedded;
-        // let clients = object.clients;
-        // console.log(clients)
+const username = 'global/cloud@apiexamples.com';
+const password = 'VMlRo/eh+Xd8M~l';
 
-})
+var url = "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/voucher";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("accept", "*/*");
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Authorization", "Basic "+btoa(username+':'+password));
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+
+document.getElementById("voucherInput").value;   
+var data = '{ "clientId":' + clientID + ', "creatingBranchId": "SE-J0emUgQnya14mOGdQS", "expiryDate": "2021-02-15T11:00:48.246Z", "issueDate": "2021-02-15T11:00:48.246Z", "originalBalance":' + balance + ', "serialNumber": 123456}';
+
+xhr.send(data);
+
 }
+
+
+
+// https://stackoverflow.com/questions/34860814/basic-authentication-using-javascript
